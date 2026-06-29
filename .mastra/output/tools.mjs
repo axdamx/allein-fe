28747b7ae1888542d5cfceef7c61d0b92499f701
axdamx <axdamx@gmail.com>
2026-06-29 16:28:59 +0800

    update gitignore

--- .mastra/output/tools.mjs
+++
@@ -1,7 +0,0 @@
-import * as tool0 from './tools/9d83eec2-4c93-41e0-8845-86be8d28c8f4.mjs';
-import * as tool1 from './tools/c2440091-9356-4064-9176-9adce8d76df7.mjs';
-import * as tool2 from './tools/2bd55a99-03c0-4c5f-9442-febfd4151877.mjs';
-import * as tool3 from './tools/14ab6e44-9400-4790-b432-15cf745be76b.mjs';
-import * as tool4 from './tools/37e35a8a-b5ff-4f85-b33e-0ac26769d8b9.mjs';
-
-                export const tools = [tool0, tool1, tool2, tool3, tool4]
--- .mastra/output/tools/c2440091-9356-4064-9176-9adce8d76df7.mjs
+++
@@ -1,9 +0,0 @@
-export { createLeadTool, createReminderTool } from './2bd55a99-03c0-4c5f-9442-febfd4151877.mjs';
-export { createClientTool, deleteClientTool, readClientsTool, updateClientTool } from './9d83eec2-4c93-41e0-8845-86be8d28c8f4.mjs';
-export { createTaskTool, readTasksTool } from './37e35a8a-b5ff-4f85-b33e-0ac26769d8b9.mjs';
-export { sendTelegramTool, sendWhatsAppTool } from './14ab6e44-9400-4790-b432-15cf745be76b.mjs';
-import '@mastra/core/tools';
-import 'zod';
-import '../service.server.mjs';
-import '@supabase/ssr';
-import 'ws';
